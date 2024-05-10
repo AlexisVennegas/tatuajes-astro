5d476ed8bb2e6f6f970a3ce6a78ff843d75367f5
AlexisVennegas <erick.alexis.venegas@gmail.com>
2024-05-10 13:54:40 +0200

    chore: Update Astro configuration to include 'output' property and fix navigation links in Tatuajes page

---
+++ src/data/tatuajes.js
@@ -0,0 +1,65 @@
+export const tatuajes = [
+    {
+        id: "tatuaje-1",
+        title: "lineal",
+        img: "https://www.lamanozurda.es/wp-content/uploads/2022/05/la.mano_.zurda_.tatuajes-madrid-ilustracion-color-andrea-magie-1-400x400.jpg",
+        desc: "El tatuaje lineal es una técnica de tatuaje que se caracteriza por la utilización de líneas finas y precisas. Se trata de un estilo de tatuaje que se ha popularizado en los últimos años y que se caracteriza por su elegancia y sencillez. Los tatuajes lineales pueden ser de diferentes estilos y diseños, desde tatuajes minimalistas y geométricos hasta tatuajes más elaborados y detallados. En cualquier caso, el tatuaje lineal es una técnica que requiere de un tatuador con experiencia y habilidad para conseguir un resultado óptimo."
+    }
+    ,
+    {
+        id: "tatuaje-2",
+        title: "Color",
+        img: "https://www.lamanozurda.es/wp-content/uploads/2022/05/la.mano_.zurda_.tatuajes-madrid-ilustracion-color-andrea-magie-2-400x400.jpg",
+        desc: "El tatuaje en color es una de las técnicas de tatuaje más populares y demandadas en la actualidad. Se trata de una técnica que consiste en la utilización de pigmentos de colores para crear diseños y dibujos en la piel. Los tatuajes en color pueden ser de diferentes estilos y diseños, desde tatuajes realistas y detallados hasta tatuajes más abstractos y creativos. En cualquier caso, el tatuaje en color es una técnica que requiere de un tatuador con experiencia y habilidad para conseguir un resultado óptimo."
+    }
+    ,
+    {
+        id: "tatuaje-3",
+        title: "Blackwork",
+        img: "https://www.lamanozurda.es/wp-content/uploads/2022/05/la.mano_.zurda_.tatuajes-madrid-ilustracion-color-andrea-magie-3-400x400.jpg",
+        desc: "El tatuaje blackwork es una técnica de tatuaje que se caracteriza por la utilización de tinta negra para crear diseños y dibujos en la piel. Se trata de un estilo de tatuaje que se ha popularizado en los últimos años y que se caracteriza por su elegancia y sencillez. Los tatuajes blackwork pueden ser de diferentes estilos y diseños, desde tatuajes minimalistas y geométricos hasta tatuajes más elaborados y detallados. En cualquier caso, el tatuaje blackwork es una técnica que requiere de un tatuador con experiencia y habilidad para conseguir un resultado óptimo."
+    },
+    {
+        id: "tatuaje-4",
+        title: "Realismo",
+        img: "https://www.lamanozurda.es/wp-content/uploads/2022/05/la.mano_.zurda_.tatuajes-madrid-ilustracion-color-andrea-magie-4-400x400.jpg",
+        desc: "El tatuaje realista es una técnica de tatuaje que se caracteriza por la utilización de sombras y luces para crear diseños y dibujos en la piel que parecen reales. Se trata de un estilo de tatuaje que se ha popularizado en los últimos años y que se caracteriza por su realismo y detalle. Los tatuajes realistas pueden ser de diferentes estilos y diseños, desde retratos de personas y animales hasta paisajes y objetos. En cualquier caso, el tatuaje realista es una técnica que requiere de un tatuador con experiencia y habilidad para conseguir un resultado óptimo."
+    },
+    {
+        id: "tatuaje-5",
+        title: "Geometrico",
+        img: "https://www.lamanozurda.es/wp-content/uploads/2022/05/la.mano_.zurda_.tatuajes-madrid-ilustracion-color-andrea-magie-5-400x400.jpg",
+        desc: "El tatuaje geométrico es una técnica de tatuaje que se caracteriza por la utilización de formas geométricas para crear diseños y dibujos en la piel. Se trata de un estilo de tatuaje que se ha popularizado en los últimos años y que se caracteriza por su elegancia y sencillez. Los tatuajes geométricos pueden ser de diferentes estilos y diseños, desde tatuajes minimalistas y abstractos hasta tatuajes más elaborados y detallados. En cualquier caso, el tatuaje geométrico es una técnica que requiere de un tatuador con experiencia y habilidad para conseguir un resultado óptimo."
+    },
+    {
+        id: "tatuaje-6",
+        title: "Acuarela",
+        img: "https://www.lamanozurda.es/wp-content/uploads/2022/05/la.mano_.zurda_.tatuajes-madrid-ilustracion-color-andrea-magie-6-400x400.jpg",
+        desc: "El tatuaje acuarela es una técnica de tatuaje que se caracteriza por la utilización de colores y formas que imitan la técnica de la acuarela. Se trata de un estilo de tatuaje que se ha popularizado en los últimos años y que se caracteriza por su creatividad y originalidad. Los tatuajes acuarela pueden ser de diferentes estilos y diseños, desde tatuajes abstractos y coloridos hasta tatuajes más realistas y detallados. En cualquier caso, el tatuaje acuarela es una técnica que requiere de un tatuador con experiencia y habilidad para conseguir un resultado óptimo."
+    },
+    {
+        id: "tatuaje-7",
+        title: "TrashPolka",
+        img: "https://www.lamanozurda.es/wp-content/uploads/2022/05/la.mano_.zurda_.tatuajes-madrid-ilustracion-color-andrea-magie-7-400x400.jpg",
+        desc: "El tatuaje trash polka es una técnica de tatuaje que se caracteriza por la utilización de manchas de tinta y elementos gráficos para crear diseños y dibujos en la piel. Se trata de un estilo de tatuaje que se ha popularizado en los últimos años y que se caracteriza por su originalidad y creatividad. Los tatuajes trash polka pueden ser de diferentes estilos y diseños, desde tatuajes abstractos y coloridos hasta tatuajes más realistas y detallados. En cualquier caso, el tatuaje trash polka es una técnica que requiere de un tatuador con experiencia y habilidad para conseguir un resultado óptimo."
+    },
+    {
+        id: "tatuaje-8",
+        title: "Neotradicional",
+        img: "https://www.lamanozurda.es/wp-content/uploads/2022/05/la.mano_.zurda_.tatuajes-madrid-ilustracion-color-andrea-magie-8-400x400.jpg",
+        desc: "El tatuaje neotradicional es una técnica de tatuaje que se caracteriza por la utilización de colores vivos y líneas gruesas para crear diseños y dibujos en la piel. Se trata de un estilo de tatuaje que se ha popularizado en los últimos años y que se caracteriza por su elegancia y sencillez. Los tatuajes neotradicionales pueden ser de diferentes estilos y diseños, desde tatuajes minimalistas y geométricos hasta tatuajes más elaborados y detallados. En cualquier caso, el tatuaje neotradicional es una técnica que requiere de un tatuador con experiencia y habilidad para conseguir un resultado óptimo."
+    },
+    {
+        id: "tatuaje-9",
+        title: "Dotwork",
+        img: "https://www.lamanozurda.es/wp-content/uploads/2022/05/la.mano_.zurda_.tatuajes-madrid-ilustracion-color-andrea-magie-9-400x400.jpg",
+        desc: "El tatuaje dotwork es una técnica de tatuaje que se caracteriza por la utilización de puntos para crear diseños y dibujos en la piel. Se trata de un estilo de tatuaje que se ha popularizado en los últimos años y que se caracteriza por su elegancia y sencillez. Los tatuajes dotwork pueden ser de diferentes estilos y diseños, desde tatuajes minimalistas y geométricos hasta tatuajes más elaborados y detallados. En cualquier caso, el tatuaje dotwork es una técnica que requiere de un tatuador con experiencia y habilidad para conseguir un resultado óptimo."
+    },
+    {
+        id: "tatuaje-10",
+        title: "Fineline",
+        img: "https://www.lamanozurda.es/wp-content/uploads/2022/05/la.mano_.zurda_.tatuajes-madrid-ilustracion-color-andrea-magie-10-400x400.jpg",
+        desc: "El tatuaje fineline es una técnica de tatuaje que se caracteriza por la utilización de líneas finas y precisas para crear diseños y dibujos en la piel. Se trata de un estilo de tatuaje que se ha popularizado en los últimos años y que se caracteriza por su elegancia y sencillez. Los tatuajes fineline pueden ser de diferentes estilos y diseños, desde tatuajes minimalistas y geométricos hasta tatuajes más elaborados y detallados. En cualquier caso, el tatuaje fineline es una técnica que requiere de un tatuador con experiencia y habilidad para conseguir un resultado óptimo."
+    }
+
+]
