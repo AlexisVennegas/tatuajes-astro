393d2ad423a785a52db8ae1631df0227f2a1eade
AlexisVennegas <erick.alexis.venegas@gmail.com>
2024-05-13 13:33:45 +0200

    Update image and menu paths in Tatuajes page and Astro configuration

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_tatuajes__YI7ZwDeG.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/_tatuajes__DTN-cr4o.mjs');
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_Cuu1cmNW.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_Dsan8A13.mjs');
+
+export { page };
