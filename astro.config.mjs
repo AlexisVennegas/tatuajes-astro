d8eaa86ada773467f1184ef105ad377d5a457552
AlexisVennegas <erick.alexis.venegas@gmail.com>
2024-05-13 10:48:07 +0200

    chore: Update navigation links in Tatuajes page and Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server'
+    output: 'server',
+    site: 'https://alexivennegas.github.io', // Cambia 'alexivennegas' por tu nombre de usuario en GitHub
+
+    // Ruta relativa a la raíz del repositorio donde se encuentra desplegado tu sitio
+    base: '/tatuajes-astro/',
 });
